de0d7adb8404ebcf3202e2b850afb0d0bec8afd4
Athfan <aththaar47@gmail.com>
2022-02-17 21:35:50 +0530

    Origin changed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://web-socket-chat-app-front-end.vercel.app/',
         methods: ['GET', 'POST']
     }
 });
