3899040b148fd1ef542ffa04b53f97cfc9ddd015
Athfan <aththaar47@gmail.com>
2022-02-17 22:29:25 +0530

    Fixed Bugs

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-const cors = require('cors');
-const {Server} = require('socket.io');
-
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 app.use(cors());
 
 const server = http.createServer(app);
 
-
-
 const io = new Server(server, {
-    cors: {
-        origin: 'https://web-socket-chat-app-front-end.vercel.app/',
-        methods: ['GET', 'POST']
-    }
+  cors: {
+    origin: "https://web-socket-chat-app-front-end.vercel.app/",
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', (socket) => {
-    console.log('User connected', socket.id);
-    
-    socket.on('joinRoom', (roomID) => {
-        socket.join(roomID);
-        console.log(`Useer with ID: ${socket.id} joined room: ${roomID}`);
-    });
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 
-    socket.on('sendMessage', (data) => {
-        socket.to(data.room).emit('receiveMessage', data);
-    })
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected', socket.id);
-    })
-})
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
 
-
-
-
-
-
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 server.listen(process.env.PORT || "0.0.0.0", () => {
-    console.log('Server listening');
-})
+  console.log("SERVER RUNNING");
+});
